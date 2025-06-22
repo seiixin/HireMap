52fdc7a326f34de41e27b40e73aef4f85f9740ae
Seiixin <sidneygarcinan@gmail.com>
2025-06-22 08:43:59 +0800

    Fix Vite base path for HTTPS

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  base: 'https://hiremap-production.up.railway.app/build/', // force HTTPS and correct base
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.jsx'],
       refresh: true,
     }),
   ],
+  base: '/build/', // <- Use relative path for production
 });
