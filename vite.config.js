ce63b1b9a07beae5ff4ccd00066d78930d1822cb
Seiixin <sidneygarcinan@gmail.com>
2025-06-22 11:22:30 +0800

    fix:dynamic base URL

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
+// Check if the current environment is production
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
+  base: isProduction ? 'https://hiremap-production.up.railway.app/build/' : '/build/',
+
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.jsx'],
@@ -10,10 +15,12 @@ export default defineConfig({
     }),
     react(),
   ],
+
   server: {
-    https: false,
+    https: false, // For local dev
     host: true,
   },
+
   build: {
     manifest: true,
     outDir: 'public/build',
