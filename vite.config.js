22f291f0492376a9fa995dc1d5dc0fd62d2b1e12
Seiixin <sidneygarcinan@gmail.com>
2025-06-22 09:25:45 +0800

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  base: 'https://hiremap-production.up.railway.app/build/',
+    base: '/build/',
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.jsx'],
