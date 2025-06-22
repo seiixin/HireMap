ac6aa94c2444352f0b302c0b9ac67f1a7106daf6
Seiixin <sidneygarcinan@gmail.com>
2025-06-22 09:11:48 +0800

    Fix mixed content

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+  base: 'https://hiremap-production.up.railway.app/build/',
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.jsx'],
       refresh: true,
     }),
   ],
-  base: '/build/', 
 });
