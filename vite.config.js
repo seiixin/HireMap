bcaaa08477fb2fb9d8af85bf54a0f3cb0123a26c
Seiixin <sidneygarcinan@gmail.com>
2025-06-22 11:05:52 +0800

    fix mixed content 11am

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react';
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  base: isProduction ? 'https://hiremap-production.up.railway.app/build/' : '/build/',
+  base: isProduction
+    ? 'https://hiremap-production.up.railway.app/build/'
+    : '/build/',
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.jsx'],
